d21b6d430cdaea37e8d78a90a957abee0a8b9ae5
songsit2017 <37284685+songsit2017@users.noreply.github.com>
2026-07-26 06:48:54 +0700

    v5 hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fuellog-v5-clean-1';
+const CACHE='fuellog-v5-clean-2';
 const CORE=['./','./index.html','./styles.css?v=5.0.0','./app.js?v=5.0.0','./firebase-config.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
